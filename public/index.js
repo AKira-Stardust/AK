e3fd826a566ae1a1eb9af75caa28821dcb94b893
Archana Kumar <archanakumar1808@gmail.com>
2023-09-30 11:33:18 +0530

    various fixes - portfolio, burger view, anchor points within page

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#small_menu");
 const wrapper = document.querySelector("#main_wrapper");
+const burger_ul_items = document.querySelectorAll(".burger-ul-item");
 
 burger.addEventListener("click", () => {
     if(menu.classList.contains("hidden")){
@@ -14,3 +15,18 @@ burger.addEventListener("click", () => {
         wrapper.classList.remove("pt-80");
     }
 });
+
+for(let burger_ul_item of burger_ul_items){
+    burger_ul_item .addEventListener("click", () => {
+        if(menu.classList.contains("hidden")){
+            menu.classList.remove("hidden");
+            wrapper.classList.remove("pt-18");
+            wrapper.classList.add("pt-80");
+    
+        } else {
+            menu.classList.add("hidden");
+            wrapper.classList.add("pt-18");
+            wrapper.classList.remove("pt-80");
+        }
+    });
+}
